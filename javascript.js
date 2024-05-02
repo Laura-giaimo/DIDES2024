acc5d2fea42a177b45fef94af42d9c0b4835ca97
laura-giaimo <laura.giaimo@stud.hslu.ch>
2024-05-02 17:24:52 +0200

    version 1.0

---
+++ javascript.js
@@ -0,0 +1,36 @@
+// Get elements from the DOM
+const wrapper = document.querySelector('.split-screen');
+const topLayer = document.querySelector('.left');
+
+// Add mousemove and touchmove event
+wrapper.addEventListener('mousemove', splitScreen);
+wrapper.addEventListener('touchmove', splitScreen);
+
+// Split screen function
+function splitScreen(e) {
+    // x coordinate variable
+    let x;
+
+    // check if it's a touch event
+    if (e.type === 'touchmove') {
+
+        // use the first touch point for simplicity
+        const touch = e.touches[0];
+
+        // set x coordinate to touch position on screen
+        x = touch.clientX;
+
+        // And if it's a mouse event
+    } else {
+        // set x coordinate to mouse position on screen
+        x = e.clientX;
+    }
+
+    // Move top layer by setting its left position to the x coordinate value we set up
+    topLayer.style.width = x + 'px';
+
+        
+
+
+
+}
