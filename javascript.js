a709d8a9d810b9ae219ffa7536f39cff61628040
laura-giaimo <laura.giaimo@stud.hslu.ch>
2024-05-05 21:07:40 +0200

    version 1.2 (try (JS return) - smartphone version)

--- javascript.js
+++ javascript.js
@@ -2,24 +2,25 @@
 const wrapper = document.querySelector('.split-screen');
 const topLayer = document.querySelector('.left');
 
-// Add mousemove and touchmove event based on screen size
-if (window.matchMedia("(max-width: 767px)").matches) {
-    wrapper.addEventListener('touchmove', splitScreenY);
-} else {
-    wrapper.addEventListener('mousemove', splitScreenX);
-}
+// Add mousemove and touchmove event
+wrapper.addEventListener('mousemove', splitScreen);
+wrapper.addEventListener('touchmove', splitScreen);
 
-// Split screen function for X axis movement
-function splitScreenX(e) {
+// Split screen function
+function splitScreen(e) {
     // x coordinate variable
     let x;
 
     // check if it's a touch event
     if (e.type === 'touchmove') {
+
         // use the first touch point for simplicity
         const touch = e.touches[0];
+
         // set x coordinate to touch position on screen
         x = touch.clientX;
+
+        // And if it's a mouse event
     } else {
         // set x coordinate to mouse position on screen
         x = e.clientX;
@@ -27,24 +28,4 @@ function splitScreenX(e) {
 
     // Move top layer by setting its left position to the x coordinate value we set up
     topLayer.style.width = x + 'px';
-}
-
-// Split screen function for Y axis movement
-function splitScreenY(e) {
-    // y coordinate variable
-    let y;
-
-    // check if it's a touch event
-    if (e.type === 'touchmove') {
-        // use the first touch point for simplicity
-        const touch = e.touches[0];
-        // set y coordinate to touch position on screen
-        y = touch.clientY;
-    } else {
-        // set y coordinate to mouse position on screen
-        y = e.clientY;
-    }
-
-    // Move top layer by setting its top position to the y coordinate value we set up
-    topLayer.style.height = y + 'px';
 }
